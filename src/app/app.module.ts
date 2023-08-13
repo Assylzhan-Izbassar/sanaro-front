ea59484f2f1fd69d6bc9409e7d5914592c47706c
Assylzhan-Izbassar <Assylzhan-Izbassar>
2023-08-13 20:01:55 +0600

    header and hero sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
